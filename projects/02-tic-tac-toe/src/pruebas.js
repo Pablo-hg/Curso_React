2661888f3cb1b7f48eb975a5a59c5402d9a4df71
phorcajada <phorcajada@lazarus.es>
2023-08-03 13:57:13 +0200

    explicación useEffect 2

--- projects/02-tic-tac-toe/src/pruebas.js
+++ projects/02-tic-tac-toe/src/pruebas.js
@@ -4,11 +4,16 @@ const { useState } = require("react");
 const Component = () => {
   const [value, setValue] = useState(false);
 
-  //"useEffect" recibe dos parámetro:
+  //"useEffect" recibe dos parámetros (el 2º es opcional):
   //1º Código para ejecutar
-  //2º Lista de las dependencias
+  //2º Lista de las dependencias --> tiene que ser un array
   //useEffect(codeToExecute, listOfDependencies)
+
+  //como mínimo se ejecutará una vez,
+  //si no hay 2º parámetro, el codigo dentro del 1º parámetro se ejecutará
+  // cada vez que se renderize el componete
+
   useEffect(() => {
     console.log("hola que tal");
-  }, listOfDependencies);
+  });
 };
