8da1b650e52391ecd02722ba16486296aca916f5
phorcajada <phorcajada@lazarus.es>
2023-08-04 14:43:56 +0200

    Explicación React.StrictMode

--- projects/03-mouse-follower/src/main.jsx
+++ projects/03-mouse-follower/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  //EL "React.StrictMode" es componente que se crea por defecto para advertirte de cosas,
+  //(como po ejemplo, codigo antiguo, codigo incorrecto)
+  //y cuando se monta el componente, ejecuta el efecto, el cleanup y otra vez el efecto.
+  //Esto lo hace para asegurar que el componente y el efecto funciona correctamente
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
