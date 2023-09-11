4145b6c81783ec9c9123b93b8738750e4c7ebc4c
phorcajada <phgpablito@gmail.com>
2023-09-11 17:46:36 +0200

    factorizamos en 2 useeffect

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './App.css';
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 export function App () {
@@ -6,24 +7,32 @@ export function App () {
     const [fact, setFact] = useState()
     const [imageURL, setimageURL] = useState()
 
-    //Al dejar el array de dependencias vacio,
-    //hacemos que solo se ejecute la 1º vez que renderiza el componente
+    //Al dejar el array de dependencias vacio, hacemos que solo se ejecute la 1º vez que renderiza el componente
+    //EFECTO QUE RECUPERA LA CITA AL CARGAR LA PÁGINA
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)  // Realiza una solicitud GET a la URL proporcionada
             .then(res => res.json())  // Convierte la respuesta ("res") en formato JSON
             .then(data => { //del objeto "data" (que son los datoa del json), accedemos al "fact"
                 const { fact } = data // del "data" recuperamos el hecho
                 setFact(fact) // establecemos el "fact"
-                // dividimos el string "fact" en un array (lo divido por espacios), obtenemos las 3 primeras posiciones y los juntamos entre espacios
-                const threeFirstWord = fact.split(' ', 3).join(' '); 
-                fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
-                .then(res => res.json())
-                .then(response => {
-                    const { url } =  response
-                    setimageURL(`https://cataas.com${url}`)
-                })
             })  
     }, []);
+
+    //EFECTO QUE RECUPERA LA IMAGEN CADA VEZ QUE TENEMOS UNA CITA NUEVA
+    //cada vez que cambia el "fact" (dependencias)....
+    useEffect(() => {
+        //si fact no es null...
+        if(!fact) return
+        // dividimos el string "fact" en un array (lo divido por espacios), obtenemos las 3 primeras posiciones y los juntamos entre espacios
+        const threeFirstWord = fact.split(' ', 3).join(' '); 
+                
+        fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+        .then(res => res.json())
+        .then(response => {
+            const { url } =  response
+            setimageURL(`https://cataas.com${url}`)
+        })
+    }, [fact])
     
 //{fact &&  <p>{fact}</p>} --> si "fact" se ha renderizado, renderizamos "fact" dentro de una etiqueta <p>
     return (
