6a3a8a55311f56cee5a2dbad3615360db4a9d569
phorcajada <phgpablito@gmail.com>
2023-09-11 17:11:01 +0200

    actualización final

---
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+export function App () {
+
+    const [fact, setFact] = useState()
+    const [imageURL, setimageURL] = useState()
+
+    //Al dejar el array de dependencias vacio,
+    //hacemos que solo se ejecute la 1º vez que renderiza el componente
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)  // Realiza una solicitud GET a la URL proporcionada
+            .then(res => res.json())  // Convierte la respuesta ("res") en formato JSON
+            .then(data => { //del objeto "data" (que son los datoa del json), accedemos al "fact"
+                const { fact } = data // del "data" recuperamos el hecho
+                setFact(fact) // establecemos el "fact"
+                // dividimos el string "fact" en un array (lo divido por espacios), obtenemos las 3 primeras posiciones y los juntamos entre espacios
+                const threeFirstWord = fact.split(' ', 3).join(' '); 
+                fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+                .then(res => res.json())
+                .then(response => {
+                    const { url } =  response
+                    setimageURL(`https://cataas.com${url}`)
+                })
+            })  
+    }, []);
+    
+
+    return (
+        <main>
+            <h1>App de Gatitos</h1>
+            {fact &&  <p>{fact}</p> }
+            {imageURL &&  <img src={imageURL} alt={`Imagen extraida usando las 3º primeras palabras de ${fact}`} /> }
+        </main>
+        
+    )
+}
